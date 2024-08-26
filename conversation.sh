8a9a646256a4a4f81e601e22b59efb376a1ea83b
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-26 21:40:16 +0530

    added converstaion script

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh. I am fine, How are you doing"
+echo "Ramesh:: I am doing good Suresh. Whats going on"
+echo "Suresh:: I started learning shell script Ramesh"
