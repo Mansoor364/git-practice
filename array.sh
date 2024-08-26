eb516f6adb3f1bef55b74cc15d6ac5a2718019a2
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-27 00:42:26 +0530

    array with indexing

---
+++ array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS=("apple" "kiwi" "banana")
+#indexing start from 0 ,size=3
+
+echo "First fruit is : ${FRUITS[0]}"
+echo "second fruit is : ${FRUITS[1]}"
+echo "third fruit is :${FRUITS[2]}"
+
+echo "all fruits are :${FRUITS[@]}
