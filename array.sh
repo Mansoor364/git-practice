56dce1021502bb82fbb253d56f77a7c2e5ba3c2a
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-27 00:44:12 +0530

    array with indexing

--- array.sh
+++ array.sh
@@ -7,4 +7,4 @@ echo "First fruit is : ${FRUITS[0]}"
 echo "second fruit is : ${FRUITS[1]}"
 echo "third fruit is :${FRUITS[2]}"
 
-echo "all fruits are :${FRUITS[@]}
+echo "all fruits are :${FRUITS[@]}"
