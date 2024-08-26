ab6796de19187de283ae46c02b9c0f234779401b
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-27 02:24:20 +0530

    special variables

--- special-var.sh
+++ special-var.sh
@@ -4,4 +4,8 @@ echo "printing all variables passed to script at run time : $@ "
 
 echo "printing no of variables passed to script at run time : $# "
 
-echo "to know your script name $0"
+echo "to know your script name : $0"
+
+echo "to know present directory : $PWD"
+
+echo "home directory of user : $HOME"
