ed369f1f488c870f34479f61bf3daa5ae00d708c
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-26 22:49:32 +0530

    variable script username n password

---
+++ 04-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "please enter your user name" 
+read  USERNAME  #takes input into variable
+echo "Username entered is :$USERNAME"
+read -s PASSWORD 
+echo "password entered is :$PASSWORD"  #displaying password entered is matched"
