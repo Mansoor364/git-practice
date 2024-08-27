4784d739313211d0428337054a765a3aa29be002
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-27 16:32:31 +0530

    conditional script

--- installation.sh
+++ installation.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
-echo "USERID is : $USERID"
+#echo "USERID is : $USERID"
+
+if [ $USERID -nt 0 ]
+then
+   echo " PLease run this script with root privilages"
+   exit 1
+fi
