2d2c2e987a5c33ea2a3178583f59ef2732e0e4a1
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-27 15:46:37 +0530

    conditional script

--- condition.sh
+++ condition.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 20 ]      #-gt greater than 
 then
-    print "$NUMBER is greater than 20"
+    echo "$NUMBER is greater than 20"
 else 
-    print "$NUMBER is less than 20"
+    echo "$NUMBER is less than 20"
 fi
