36dbe46658dec4393d8a2a8b289fe2dc04544e65
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-28 16:36:18 +0530

    git n mysql installtion

--- installation.sh
+++ installation.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
 USERID=$(id -u)
-#echo "USERID is : $USERID"
-
-if [ $USERID -nt 0 ]
+if [ $USERID -ne 0 ]
 then
-   echo " PLease run this script with root privilages"
-   exit 1
+   echo "Please run the script with root user privilageous"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then 
+   echo "Installing git for u.."
+   dnf install git -y
+   if [ $? -ne 0 ]
+   then
+      echo "Git installation is not successfull"
+      exit 1
+   else
+      echo "Git installtion is successfull.."
+   fi
+else
+   echo "Git is already installed"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+   echo "MYSQL installation is in process"
+   dnf install mysql -y
+   if [ $? -ne 0 ]
+   then
+      echo "MYSQL installation is not successfull"
+      exit 1
+   else      
+      echo "MYSQL is successfully installed"
+   fi
+else
+   echo "MYSQL is already installed "
 fi
