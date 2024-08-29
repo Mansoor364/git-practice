405797c08462c6f8c32d4b2b4887cfc6d877d837
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-29 20:16:27 +0530

    enter number in runtime

--- installation.sh
+++ installation.sh
@@ -7,11 +7,18 @@ then
     exit 1
 fi
 
-dnf list installed git 
+dnf list installed gitttt
  if [ $? -ne 0 ]
  then
      echo "Git is not installed  going to install it.."
      dnf install Git -y
+     if [ $? -ne 0 ]
+     then 
+         echo " Git installation is not successful check it.."
+         exit 1
+    else
+        echo " Git is installed successfully "
+    fi
 else 
     echo "Git is already installed nothing to do "
  fi
