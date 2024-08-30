aff147bdac67c8bca3d58dee22d1db49545b5425
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-30 11:10:49 +0530

    redirecting output to file

--- redirectors.sh
+++ redirectors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ls -l > output.txt #redirecting output into file
-cat output.sh
+cat output.txt
 
 ls -l 1>output.txt #redirecting success of command into file
 cat output.txt
@@ -12,7 +12,7 @@ lss -l &>output.txt #redirecting both success or failure into file
 cat output.txt
 
 ls -l&>>output.txt #appending status of command in file
-cat output.sh
+cat output.txt
 
 
 
