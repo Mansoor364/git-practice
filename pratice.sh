8152e2ec80b907fe18703b8beb6650da4be73d5e
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-30 14:53:47 +0530

    practice

--- pratice.sh
+++ pratice.sh
@@ -3,7 +3,7 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
-mkdir -p $LOGS_FILE
+mkdir -p $LOGS_FOLDER
 
 R="\e[31m"
 G="\e[32m"
@@ -50,4 +50,5 @@ do
         VALIDATE $? "$package"
     else
         echo -e "$package $Y is already installed $N " &>>$LOG_FILE
+    fi
 done
