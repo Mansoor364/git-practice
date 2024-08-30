768b44dfa3c35798b7ee57b9806c6255b3919919
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-30 12:27:19 +0530

    redirections with logs

---
+++ 01redirectors.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0) | cut -d "." -f1
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+USAGE () {
+    echo -e " $R USAGE:: $N sudo sh 01-redirectors.sh package1 package2 ... "
+    exit 1
+}
+
+CHECK_ROOT 
+
+if [ $# -eq 0 ]
+then
+    USAGE
+fi
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "$R Please run the script with root user privilageous $N" &>>$LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is $R Failed.. $N" &>>LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is $G successful.. $N" &>>LOG_FILE
+    fi
+}
+
+# sh loops.sh git mysql postfix nginx
+for package in $@  # $@ -- all arguments passed to it
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then 
+        echo " Installing $package for u.." &>>$LOG_FILE
+        dnf install $package -y  &>>$LOG_FILE
+        VALIDATE $? "$package"   
+    else
+        echo -e" $Y $package is already installed $N" &>>$LOG_FILE
+    fi
+done
