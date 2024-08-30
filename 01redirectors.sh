6d486f18d3817b519777208428aa4ed9a00921e8
mansoor shaik <mansoorshaik364@gmail.com>
2024-08-30 12:50:34 +0530

    redirections with logs

--- 01redirectors.sh
+++ 01redirectors.sh
@@ -4,7 +4,6 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
-
 mkdir -p $LOGS_FOLDER
 
 R="\e[31m"
